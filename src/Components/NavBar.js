83eb06c9157d64e79b4354881d1617ec7c34aea9
Cefe fabian <cefefabian6@gmail.com>
2022-08-25 09:16:06 -0300

    NavBar

---
+++ src/Components/NavBar.js
@@ -0,0 +1,16 @@
+const NavBar = () =>{
+    return(
+        <header className='header'>
+        <div className='div-marca'>
+          <h1>logo</h1>
+        </div>
+        <div className='links'>
+          <a className="a" href='https://plataforma.coderhouse.com/cursos/34745/reactjs'>Carrito</a>
+          <a className="a" href='https://plataforma.coderhouse.com/cursos/34745/reactjs'>Carrito</a>
+          <a className="login" href='https://plataforma.coderhouse.com/cursos/34745/reactjs'>login</a>
+        </div>
+      </header>
+    )
+}
+
+export default NavBar
